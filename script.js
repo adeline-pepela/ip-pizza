db5f8159038dded1b263b11a9c782343782d0e32
adeline-pepela <adelinemakokha@gmail.com>
2022-03-19 08:22:00 +0300

    adding jquery cdn on html file

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ function Getpizza( name,size,crust,topping, total ){
 
 // proceed button
 $(document).ready(function(){
-  // $("button.proceed").click(function(){
-  //   $("button.proceed").hide();
-  //   $("#information").hide();
-  //   $("div.choise").slideDown(1000);
-  // });
+   $("button.proceed").click(function(){
+   $("button.proceed").hide();
+   $("#information").hide();
+   $("div.choise").show();
+   });
   $("button.proceed").click(function(event){
+  
    let pname = $(".name option:selected").val();
    let psize = $("#size option:selected").val();
    let pcrust = $("#crust option:selected").val();
